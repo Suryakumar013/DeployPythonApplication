7fd603fe50478b7a6cbef16f7d1718f985549b9b
Suryakumar013 <gsuryabron@gmail.com>
2024-09-26 13:53:48 +0530

    Update Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -15,4 +15,4 @@ num = int(input("Enter a number: "))
 if is_armstrong(num):
     print(f"{num} is an Armstrong number.")
 else:
-print(f"{num} is not an Armstrong number.")
+    print(f"{num} is not an Armstrong number.")
